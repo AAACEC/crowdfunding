0e4810fe71d872eb33d5bedc6ea2b8ee6c796863
rmobis <r.mobis@gmail.com>
2016-01-30 02:04:44 -0200

    Update rewards

--- js/abbrTooltips.js
+++ js/abbrTooltips.js
@@ -3,9 +3,9 @@
 
 	const TOOLTIP_MESSAGES = {
 		'Xupita': 'Presidente da AAACEC - Gestão 2016',
-		'Valeska': 'Vice-Presidente da AAACEC - Gestão 2016',
 		'Espaço Computação Alumni': 'Espaço no Manual dos Bixos dedicado aos ilustríssimos doadores deste projeto. Cada doador terá uma foto, nome, apelido, ano e descrição.',
-		'agasalho AAACEC 2016': 'Entregas previstar para Maio de 2016.',
+		'agasalho AAACEC 2016': 'Entregas previstas para Maio de 2016.',
+        'miniatura do bandeirão': 'Escala de aproximadamente 1:10.',
 		'miniatura do 100Nossão': 'Escala de aproximadamente 1:10.',
 	};
 
