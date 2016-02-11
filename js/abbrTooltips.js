24a9aa8fad94bfd854bea7d3eab7274a6c04e65c
Rodrigo Dahlstrom Hilkner <rodrigo.hilkner@gmail.com>
2016-02-10 22:33:59 -0200

    Updated PDF and texts of 'A Campanha' and 'Espaço Computação Alumni'

--- js/abbrTooltips.js
+++ js/abbrTooltips.js
@@ -3,7 +3,7 @@
 
 	const TOOLTIP_MESSAGES = {
 		'Xupita': 'Presidente da AAACEC - Gestão 2016',
-		'Espaço Computação Alumni': 'Caderno entregue junto ao Kit Bixo destinado aos nossos ilustríssimos doadores do projeto de Crowdfunding. Cada doador terá uma foto, nome, apelido, ano e descrição. Obs.: válido apenas para doações até 13/02.',
+		'Espaço Computação Alumni': 'Caderno entregue junto ao Kit Bixo destinado aos nossos ilustríssimos doadores do projeto de Crowdfunding. Cada doador terá uma foto, nome, apelido, ano e descrição. Obs.: válido apenas para doações até 16/02.',
 		'agasalho AAACEC 2016': 'Entregas previstas para maio/2016.',
         'miniatura do bandeirão': 'Escala de aproximadamente 1:10.',
 		'miniatura do 100Nossão': 'Escala de aproximadamente 1:10.',
